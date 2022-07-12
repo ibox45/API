909b4b7e6385f9bbdfedd57f9677a7181d104d49
ibox45 <ibrahimakdeniz@gmail.com>
2022-07-12 15:22:13 +0300

    API

---
+++ src/test/java/BaseUrls.java
@@ -0,0 +1,16 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class BaseUrls {
+
+    protected  RequestSpecification spec;
+
+    //@Before annotation'ı kullandıgmız methodlar her test methodundan once calıstırılır.
+    @Before
+    public void setUp(){
+
+        spec=new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
+    }
+
+}
